6005e6fef88c5cb889da8e5d7cd70fbd1f899b5c
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2015-12-10 10:47:33 +0100

    Importati sorgenti da tutorial esempio INDI

---
+++ driver/indi-gapers.h
@@ -0,0 +1,33 @@
+#ifndef GAPERSSCOPE_H
+#define GAPERSSCOPE_H
+/*
+   INDI Developers Manual
+   Tutorial #2
+   "Simple Telescope Driver"
+   We develop a simple telescope simulator.
+   Refer to README, which contains instruction on how to build this driver, and use it
+   with an INDI-compatible client.
+*/
+#include "indibase/inditelescope.h"
+class GapersScope : public INDI::Telescope
+{
+public:
+    GapersScope();
+protected:
+    // General device functions
+    bool Connect();
+    bool Disconnect();
+    const char *getDefaultName();
+    bool initProperties();
+    // Telescoe specific functions
+    bool ReadScopeStatus();
+    bool Goto(double,double);
+    bool Abort();
+private:
+    double currentRA;
+    double currentDEC;
+    double targetRA;
+    double targetDEC;
+    unsigned int DBG_SCOPE;
+};
+#endif // GAPERSSCOPE_H
